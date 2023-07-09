78fda57b28bc41eb3d945ae9e4edfb44e7139e8a
de-mark <data.engineer.mark@gmail.com>
2023-07-08 19:55:52 -0700

    Switched to dark mode

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,12 @@
-let defaultMap = L.tileLayer( "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'",
-{
-    attribution:
-    'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+let defaultMap = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',{
+    attribution: '© OpenStreetMap contributors, © CartoDB'
 });
+
+// let defaultMap = L.tileLayer( "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'",
+// {
+//     attribution:
+//     'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+// });
 
 let baseMaps = {
     "Default Map": defaultMap,
